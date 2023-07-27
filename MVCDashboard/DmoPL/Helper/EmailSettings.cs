d79505f05bef20832b342b61baef2fb04e955b14
unknown <hassanmostafa01094977@gmail.com>
2023-07-27 18:57:18 +0300

    first version of MVCApp

---
+++ MVCDashboard/DmoPL/Helper/EmailSettings.cs
@@ -0,0 +1,21 @@
+﻿using DemoDAL.Models;
+using System.Net;
+using System.Net.Mail;
+
+namespace DmoPL.Helper
+{
+	public static class EmailSettings
+	{
+		public static void SendEmail(Email email)
+		{
+			//var client = new SmtpClient("linkdev.com", 444);
+			//client.EnableSsl= true;
+			//client.Credentials = new NetworkCredential("email@linkdev.com", "password");
+			//client.Send("email@linkdev.com", email.To, email.Subject, email.Body);
+			var client = new SmtpClient("smtp.gmail.com", 587);
+			client.EnableSsl= true;
+			client.Credentials = new NetworkCredential("khaledelnassag@gmail.com", "tnvtmvqczntkoqwl");
+			client.Send("khaledelnassag@gmail.com", email.To, email.Subject, email.Body);
+		}
+	}
+}
